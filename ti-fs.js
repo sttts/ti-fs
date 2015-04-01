1341f88d39506e5241a7dbd0e11cd599ac84773a
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2015-04-01 09:43:28 +0200

    Compile ti-fs.js

--- ti-fs.js
+++ ti-fs.js
@@ -27,69 +27,40 @@ if (typeof Object.create === 'function') {
 // shim for using process in browser
 
 var process = module.exports = {};
+var queue = [];
+var draining = false;
 
-process.nextTick = (function () {
-    var canSetImmediate = typeof window !== 'undefined'
-    && window.setImmediate;
-    var canMutationObserver = typeof window !== 'undefined'
-    && window.MutationObserver;
-    var canPost = typeof window !== 'undefined'
-    && window.postMessage && window.addEventListener
-    ;
-
-    if (canSetImmediate) {
-        return function (f) { return window.setImmediate(f) };
+function drainQueue() {
+    if (draining) {
+        return;
     }
-
-    var queue = [];
-
-    if (canMutationObserver) {
-        var hiddenDiv = document.createElement("div");
-        var observer = new MutationObserver(function () {
-            var queueList = queue.slice();
-            queue.length = 0;
-            queueList.forEach(function (fn) {
-                fn();
-            });
-        });
-
-        observer.observe(hiddenDiv, { attributes: true });
-
-        return function nextTick(fn) {
-            if (!queue.length) {
-                hiddenDiv.setAttribute('yes', 'no');
-            }
-            queue.push(fn);
-        };
+    draining = true;
+    var currentQueue;
+    var len = queue.length;
+    while(len) {
+        currentQueue = queue;
+        queue = [];
+        var i = -1;
+        while (++i < len) {
+            currentQueue[i]();
+        }
+        len = queue.length;
     }
-
-    if (canPost) {
-        window.addEventListener('message', function (ev) {
-            var source = ev.source;
-            if ((source === window || source === null) && ev.data === 'process-tick') {
-                ev.stopPropagation();
-                if (queue.length > 0) {
-                    var fn = queue.shift();
-                    fn();
-                }
-            }
-        }, true);
-
-        return function nextTick(fn) {
-            queue.push(fn);
-            window.postMessage('process-tick', '*');
-        };
+    draining = false;
+}
+process.nextTick = function (fun) {
+    queue.push(fun);
+    if (!draining) {
+        setTimeout(drainQueue, 0);
     }
-
-    return function nextTick(fn) {
-        setTimeout(fn, 0);
-    };
-})();
+};
 
 process.title = 'browser';
 process.browser = true;
 process.env = {};
 process.argv = [];
+process.version = ''; // empty string to avoid regexp issues
+process.versions = {};
 
 function noop() {}
 
@@ -110,6 +81,7 @@ process.cwd = function () { return '/' };
 process.chdir = function (dir) {
     throw new Error('process.chdir is not supported');
 };
+process.umask = function() { return 0; };
 
 },{}],3:[function(require,module,exports){
 module.exports = function isBuffer(arg) {
@@ -873,8 +845,13 @@ fs.open = function open(path, flags, mode, callback) {
 
 fs.openSync = function openSync(path, flags, mode) {
 	var tiMode = assertFlags(flags),
-		file = $F.getFile(path),
-		fd = file.open(tiMode);
+		file = $F.getFile(path);
+
+	if (tiMode === $F.MODE_APPEND && !fs.existsSync(path)) {
+		file.open($F.MODE_WRITE).close();
+	}
+
+	var fd = file.open(tiMode);
 	fd.__path = path;
 	return fd;
 };
@@ -957,7 +934,6 @@ fs.rename = function rename(oldPath, newPath, callback) {
 			good = false;
 		try {
 			good = $F.getFile(oldPath).move(newPath);
-			console.log('good: ' + good);
 			if (!good) {
 				err = new Error('could not move file');
 			}
